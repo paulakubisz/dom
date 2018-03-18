5f5f8f0e6ad2940ddc209ae052991043ca4123b3
Paulina Kubisz <paulina.kubisz@airhelp.com>
2018-03-18 18:37:24 +0100

    Gra, reszta zadan

--- Dzien4-5_dom/zadania_listy/zad5.py
+++ Dzien4-5_dom/zadania_listy/zad5.py
@@ -2,5 +2,5 @@
 lista_a = [10,20,30,20,10,50,60,40,80,50,40]
 
 
-print(list(set(lista_a)))  #usuwanie duplikatow
+print(list(set(lista_a)))
 
--- Dzien4-5_dom/zadania_listy/zad7.py
+++ Dzien4-5_dom/zadania_listy/zad7.py
@@ -1,2 +1,20 @@
 # program sprawdza czy dwie listy maja co najmniej jeden wspolny element,
 # jesli tak wypisuje True
+
+import collections
+x = ['c', 'b']
+y = ['b', 'c']
+
+if collections.Counter(x) == collections.Counter(y):
+    print(True)
+else:
+    print(False)
+
+
+print("---B---")
+
+A = {1, 2, 3}
+B = {1, 2, 3, 4, 5}
+C = {1, 2, 4, 5}
+
+print(A.issubset(B))
